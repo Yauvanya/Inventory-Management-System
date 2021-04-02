b9090d54b0b171c74ae6cfb237a2eb5d9931e58f
Nibhrit-Garg <nibhgarg12@gmail.com>
2021-04-02 21:45:49 +0530

    added model names

--- inventoryManagementSystem/IMS/models.py
+++ inventoryManagementSystem/IMS/models.py
@@ -2,5 +2,13 @@
 from django.db import models
 
 # Create your models here.
-class rawMaterials(models.Model):
+class materialClass(models.Model):
+    packaging_materials = models.CharField(max_length=100)
+    raw_materials = models.CharField(max_length=100)
+
+class materials(models.Model):
     name = models.
+
+class packagedProducts(models.Model):
+
+class shippingProducts(models.Model):
