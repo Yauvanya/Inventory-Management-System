69913b306164d6d7d6b294499c9c70d380faf783
metreaux <gurpreetsingh.19july@gmail.com>
2021-04-12 21:19:09 +0530

    added models
    added models

--- inventoryManagementSystem/IMS/models.py
+++ inventoryManagementSystem/IMS/models.py
@@ -3,4 +3,23 @@ from django.db import models
 
 # Create your models here.
 class rawMaterials(models.Model):
-    name = models.
+    id = models.CharField(primary_key=True)
+    mat_name    = models.CharField()
+    quantity_present = models.CharField()
+    units = models.CharField()
+
+
+class packagedProducts(models.Model):
+    packaged_product_id = models.CharField()
+    name = models.TextField()
+    quantity_present = models.CharField()
+
+class ingredeints(models.Model):
+    RawMatId = models.ForeignKey(packagedProducts)
+    quantity_used = models.CharField()
+    units = models.CharField()
+
+class packagingMaterials(models.Model):
+    packMatId = models.ForeignKey(packagedProducts)
+    quantity_used = models.CharField()
+    units = models.CharField()
