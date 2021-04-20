65aa92ade1b83e397547613c1f5a4fdbb104826d
Nibhrit-Garg <nibhgarg12@gmail.com>
2021-04-20 22:40:05 +0530

    fixed models

--- inventoryManagementSystem/IMS/models.py
+++ inventoryManagementSystem/IMS/models.py
@@ -2,34 +2,55 @@
 from django.db import models
 
 # Create your models here.
-class rawMaterials(models.Model):
-    id = models.CharField(primary_key=True)
-    mat_name    = models.CharField()
-    quantity_present = models.CharField()
-    units = models.CharField()
 
-
-class packagedProducts(models.Model):
-    packaged_product_id = models.CharField()
-    name = models.TextField()
-    quantity_present = models.CharField()
-
-class ingredeints(models.Model):
-    RawMatId = models.ForeignKey(packagedProducts)
-    quantity_used = models.CharField()
-    units = models.CharField()
-
-class packagingMaterials(models.Model):
-    packMatId = models.ForeignKey(packagedProducts)
-    quantity_used = models.CharField()
-    units = models.CharField()
 class materialClass(models.Model):
-    packaging_materials = models.CharField(max_length=100)
-    raw_materials = models.CharField(max_length=100)
-
-class materials(models.Model):
-    name = models.
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100,unique=True)
+    
+class rawMaterials(models.Model): 
+    UNITS_CHOICES = [
+        (1,"mg"),
+        (2,"g"),
+        (3,"kg"),
+        (4,"ml"),
+        (5,"l")
+    ]       
+    id = models.CharField(primary_key=True)
+    material_class = models.ForeignKey(materialClass)
+    mat_name = models.CharField(max_length=100,unique=True)
+    quantity_present = models.FloatField(max_length=100)
+    units = models.CharField(choices=UNITS_CHOICES)
 
 class packagedProducts(models.Model):
+    UNITS_CHOICES = [
+        (1,"mg"),
+        (2,"g"),
+        (3,"kg"),
+        (4,"ml"),
+        (5,"l")
+    ] 
+    id = models.AutoField(primary_key=True)
+    name = models.TextField(max_length=100,unique=True)
+    ingredient = models.ManytoManyField(rawMaterials)
+    quantity_present = models.FloatField()
+    units = models.CharField(choices=UNITS_CHOICES)
 
-class shippingProducts(models.Model):
+#seller info and input raw materials
+
+
+
+
+# class ingredeints(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     RawMatId = models.ForeignKey(packagedProducts)
+#     quantity_used = models.CharField()
+#     units = models.CharField()
+
+
+
+# class materials(models.Model):
+#     name = models.
+
+# class packagedProducts(models.Model):
+
+# class shippingProducts(models.Model):
